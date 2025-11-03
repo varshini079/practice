6356d269ef657ca5906b1df0692e961b5b109b3e
Donga Varshini Devi <104311746+varshini079@users.noreply.github.com>
2025-11-03 14:02:40 +0530

    Update Manager.java
    methods added

--- Manager.java
+++ Manager.java
@@ -5,7 +5,17 @@ public class Manager extends Designation implements Role{
     @Override
     public void performDuties() {
         // TODO Auto-generated method stub
+        
         throw new UnsupportedOperationException("Unimplemented method 'performDuties'");
+    }
+   
+    @Override 
+    public double calculateBonus(){
+        
+    }
+    public double calculateSalary(){
+        
     }
     
 }
+
