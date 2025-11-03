65c6eeda28a697377b4b2baab5610b5e5c4b088b
Donga Varshini Devi <104311746+varshini079@users.noreply.github.com>
2025-11-03 11:59:54 +0530

    Update Employee.java
    setters are added

--- Employee.java
+++ Employee.java
@@ -4,6 +4,15 @@ public class Employee {
     private String name;
     private int ID;
     private String department;
+        public void setName(String name) {
+        this.name = name;
+    }
+    public void setID(int iD) {
+        ID = iD;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
     public Employee(String name,int ID,String department){
         this.name=name;
         this.ID=ID;
@@ -23,4 +32,5 @@ public class Employee {
     System.out.println("Employee name: "+name+"\n Employee ID: "+ID+"\n Department: "+department);
   }
 }
+
 
