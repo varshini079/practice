209a852f67f818daf13a0affd51f3a407c8a88ca
Donga Varshini Devi <104311746+varshini079@users.noreply.github.com>
2025-11-03 12:01:49 +0530

    Update Designation.java
    methods added

--- Designation.java
+++ Designation.java
@@ -1,10 +1,27 @@
 package sample;
 
-class Designation extends Employee{
+abstract class Designation extends Employee{
 
-    public void performDuties() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'performDuties'");
+    // public void performDuties() {
+    //     // TODO Auto-generated method stub
+    //     throw new UnsupportedOperationException("Unimplemented method 'performDuties'");
+    // }
+
+    String position;
+    int level;
+   public Designation(String name, String id, String department, String position, String level) {
+        super(name, id, department);
+        this.position = position;
+        this.level = level;
     }
+
+    public String getPosition(){
+        return position;
+    }
+    public int getLevel(){
+        return level;
+    }
+public abstract double calculateBonus();
+    public abstract String performDuties();
 
 }
